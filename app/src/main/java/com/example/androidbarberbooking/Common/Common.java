4e3b2210e9801b798f2b159d744a757f891a96d0
Đặng Anh  Đức <anhduc.dang@my.jcu.edu.au>
2019-06-01 01:08:54 +0800

    initial commit

---
+++ app/src/main/java/com/example/androidbarberbooking/Common/Common.java
@@ -0,0 +1,9 @@
+package com.example.androidbarberbooking.Common;
+
+import com.example.androidbarberbooking.Model.User;
+
+public class Common {
+
+    public static String IS_LOGIN="IsLogin";
+    public static User currentUser;
+}
